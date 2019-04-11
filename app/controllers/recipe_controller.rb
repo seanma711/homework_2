6ef53399ac2fd0d0a014bdf2c4a495f104ff2a66
Sean Ma <logiclegolas@gmail.com>
2019-04-11 08:02:04 -0400

    Made the show page
    Created the show html page and the controller for show

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -2,4 +2,8 @@ class RecipeController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+  def show
+    @recipe = Recipe.find(params[:id])
+    @ingredients = @recipe.ingredients
+  end
 end
